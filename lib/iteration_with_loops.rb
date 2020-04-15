06e9136747ab17f417cefebcaf77f283b87096b8
Jeanmarie Jackman <jeanmarie.jackman@gmail.com>
2020-04-15 02:35:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ count = 0
   
     while element_index < src[row_index].count do
       if src[row_index][element_index] == src[row_index][element_index].to_s
-      new_array << [row_index][element_index]
+      new_array << [row_index][element_index].join 
     end 
     
     element_index += 1
